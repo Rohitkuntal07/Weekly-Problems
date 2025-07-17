263a28b97e192ae9ff7b51c404bc0efdbdf633f3
Rohitkuntal07 <rohitkuntal315@gmail.com>
2025-07-17 16:33:58 +0530

    Update SubArraySumK.java
    Added the most efficient method to solve Problem 2

--- week1/Problem2/SubArraySumK.java
+++ week1/Problem2/SubArraySumK.java
@@ -1,1 +1,34 @@
+// Using Prefix Sum + HashMap
+
+import java.util.*;
+
+public class SubArraySumK {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();         // number of days
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();   // calories burned per day
+        }
+
+        int k = sc.nextInt();         // target calories
+        int sum = 0, count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);                // base case
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        System.out.println(count);
+    }
+}
+
 
