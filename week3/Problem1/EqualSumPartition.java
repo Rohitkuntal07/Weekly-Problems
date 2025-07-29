d02a798c2d72b5c5707696ac4a6825a922bf0692
Rohitkuntal07 <rohitkuntal315@gmail.com>
2025-07-29 19:38:25 +0530

    Create EqualSumPartition.java
    added the solution for problem 1 of week 3

---
+++ week3/Problem1/EqualSumPartition.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class EqualSumPartition {
+
+    public static boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        return dp[target];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().trim().split("\\s+|,");
+        int[] nums = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            nums[i] = Integer.parseInt(input[i]);
+        }
+        System.out.println(canPartition(nums));
+    }
+}
