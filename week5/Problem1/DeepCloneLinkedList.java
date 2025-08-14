031c69dfe6e0ab4516636382bd822e0fd3fdf981
Rohitkuntal07 <rohitkuntal315@gmail.com>
2025-08-14 10:19:06 +0530

    Create DeepCloneLinkedList.java
    solution to problem 1 of week 5

---
+++ week5/Problem1/DeepCloneLinkedList.java
@@ -0,0 +1,22 @@
+public class Main {
+    public Node DeepCloneLinkedList(Node head) {
+        Map<Node, Node> hashMap = new HashMap<>();
+        Node cur = head;
+
+        while (cur != null) {
+            hashMap.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+
+        cur = head;
+
+        while (cur != null) {
+            Node copy = hashMap.get(cur);
+            copy.next = hashMap.get(cur.next);
+            copy.random = hashMap.get(cur.random);
+            cur = cur.next;
+        }
+
+        return hashMap.get(head);        
+    }
+}
