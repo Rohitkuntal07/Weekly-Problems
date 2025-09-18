b34db2254d60188483fc803125cfdcf0225890cb
Rohit <rohitkuntal315@gmail.com>
2025-09-18 20:48:30 +0530

    Create LongestSubsequence.java
    added solution to Problem 3 of week 10

---
+++ week10/Problem3/LongestSubsequence.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        for (int num : nums) {
+            int idx = Collections.binarySearch(temp, num);
+            if (idx < 0) idx = -(idx + 1);
+            if (idx == temp.size()) temp.add(num);
+            else temp.set(idx, num);
+        }
+        return temp.size();
+    }
+}
